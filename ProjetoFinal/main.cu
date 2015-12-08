5f12a6781052c5a93b831174d38d008d9a0058d8
Colgath <igorsbaliza@usp.br>
2015-12-08 07:45:27 -0200

    Quase

--- ProjetoFinal/main.cu
+++ ProjetoFinal/main.cu
@@ -1,19 +1,32 @@
 #include <iostream>
 #include <string>
 #include <stdio.h>
-#include <opencv/cv.h>
+
+//#include <opencv2/opencv.hpp>
+//#include <opencv2/core/cuda.hpp>
+
+
+#include <opencv/cv.h> //Minha versão é outra.
 #include <opencv/ml.h>
 #include <opencv/cxcore.h>
 #include <opencv/highgui.h>
-#include <sys/time.h>
-#include <cuda.h>
+
+
+//#include <sys/time.h> //Não existe no Windows.
+
+
+//#include <cuda.h> //Original
+
+#include <CUDA/cuda.h>
+#include <CUDA/cuda_runtime.h>
+#include <CUDA/device_launch_parameters.h>
 
 __global__ void smoothGray (unsigned char *imagem, unsigned char *saida, unsigned int cols, unsigned int linhas)
 {
 	unsigned int indice = (blockIdx.y * blockDim.x * 65536) + (blockIdx.x * 1024) + threadIdx.x; // calcula o indice do vetor com base nas dimensões de bloco e indice da thread
 	
 	//indices para o campo da imagem que participará do smooth 
-	int i_begin = (indice/(int)cols)-2, i_end = (indice/(int)cols)+2);
+	int i_begin = (indice/(int)cols) - 2, i_end = (indice/(int)cols)+2;
 	int j_begin = (indice%(int)cols) - 2, j_end = (indice%(int)cols)+2;
 	if(i_begin<0) i_begin = 0;
 	if(j_begin<0) j_begin = 0;
@@ -36,13 +49,18 @@ __global__ void smoothGray (unsigned char *imagem, unsigned char *saida, unsigne
 
 void cudaCinza(char *nome_imagem, char *nome_saida)
 {
-	cv::Mat *imagem = cv::imread(nomeImagem, CV_LOAD_IMAGE_GRAYSCALE); //abre a imagem de origem
-	cv::Mat *saida(imagem->rows, imagem->cols, imagem.type()); //cria a imagem de destino
+	cv::Mat imagem = cv::imread(nome_imagem, CV_LOAD_IMAGE_GRAYSCALE); //abre a imagem de origem
+	cv::Mat saida(imagem.rows, imagem.cols, imagem.type()); //cria a imagem de destino
+
+	
+	struct timeval start,end;
+    double tempo=0.0;
+    gettimeofday(&start,NULL);
 	
 	unsigned char * imagem_entrada;
 	unsigned char * imagem_saida;
-	cudaMalloc((void **)&(imagem_entrada), sizeof(unsigned char) * imagem->rows*imagem->cols);//aloca a imagem de origem na GPU
-	cudaMemcpy((void *)(imagem_entrada), (void *)(imagem->data), sizeof(unsigned char) * imagem->rows * imagem->cols, cudaMemcpyKind::cudaMemcpyHostToDevice); //manda a imagem de origem pra GPU
+	cudaMalloc((void **)&(imagem_entrada), sizeof(unsigned char) * imagem.rows*imagem.cols);//aloca a imagem de origem na GPU
+	cudaMemcpy((void *)(imagem_entrada), (void *)(imagem.data), sizeof(unsigned char) * imagem.rows * imagem.cols, cudaMemcpyKind::cudaMemcpyHostToDevice); //manda a imagem de origem pra GPU
 	
 	cudaMalloc((void **)&(imagem_saida), sizeof(unsigned char) * imagem.rows*imagem.cols);//aloca a imagem de destino na GPU
 	
@@ -50,28 +68,121 @@ void cudaCinza(char *nome_imagem, char *nome_saida)
 	/* Inicio do processo paralelo */
 	
 	cuInit(0); 
-	dim3 Bloco_dim(1, 1024); //define bloco de 1 linha tamanho 1024
-	unsigned int num_Blocos = (unsigned int)ceil(((double)(imagem->rows*imagem->cols))/1024); //verifica quantos blocos serão necessário para a imagem
-	smoothGray <<num_Blocos, Bloco_dim>> (imagem_entrada, imagem_saida, imagem->cols, imagem->rows);
+	dim3 Bloco_dim(1024); //define bloco de 1 linha tamanho 1024
+	unsigned int num_Blocos = (unsigned int)ceil(((double)(imagem.rows*imagem.cols))/1024); //verifica quantos blocos serão necessário para a imagem
+	smoothGray <<< num_Blocos, Bloco_dim >>> (imagem_entrada, imagem_saida, imagem.cols, imagem.rows);
 	cudaDeviceSynchronize(); 
 	
 	/* Fim do processo paralelo */
 	
 	
-	cudaMemcpy((void *)(saida->data), imagem_saida,(size_t)(sizeof(unsigned char) * imagem->rows * imagem->cols), cudaMemcpyKind::cudaMemcpyDeviceToHost); //copia a imagem resultante para o Host
+	cudaMemcpy((void *)(saida.data), imagem_saida,(size_t)(sizeof(unsigned char) * imagem.rows * imagem.cols), cudaMemcpyKind::cudaMemcpyDeviceToHost); //copia a imagem resultante para o Host
+	
+	gettimeofday(&end,NULL);
+    tempo =( ((double) ( ((end.tv_sec * 1000000 + end.tv_usec)
+                                - (start.tv_sec * 1000000 + start.tv_usec))))/1000000);
+
+    char nome[100];
+    sprintf(nome, "%s.out", imagem_saida);
+    FILE *fp = NULL;
+    if((fp = fopen(nome, "a")) == NULL)
+		fp = fopen(nome,"w");
+   
+    fprintf(fp, "%lf", tempo);
+    //apresentar os resultado
+    fclose(fp);
+	
 	
 	cv::imwrite(nome_saida, saida); //Escreve imagem no arquivo
 	
 	cudaFree(imagem_entrada);
 	cudaFree(imagem_saida);
-	delete imagem;
-	delete saida;
 }
 
+__global__ void smoothColor (unsigned char *imagem, unsigned char *saida, unsigned int cols, unsigned int linhas)
+{
+	unsigned int indice = (blockIdx.y * blockDim.x * 65536) + (blockIdx.x * 1024) + threadIdx.x; // calcula o indice do vetor com base nas dimensões de bloco e indice da thread
+
+	//indices para o campo da imagem que participará do smooth 
+	int i_begin = (indice/(int)cols)-2, i_end = (indice/(int)cols)+2;
+	int j_begin = (indice%(int)cols)-2, j_end = (indice%(int)cols)+2;
+	if(i_begin<0) i_begin = 0;
+	if(j_begin<0) j_begin = 0;
+	if(i_end>=cols) i_end = cols-1;
+	if(j_end>=cols) j_end = cols-1;
+	
+	//calcula o smooth no ponto de indice da thread
+	int media[3] = {0,0,0};
+	int qtd = 0;
+	for (int i = i_begin; i<=  i_end; ++i)
+	{
+		for(int j = j_begin; j<= j_end; ++j)
+		{
+			media[0] += imagem[((i*cols)+j)*3];
+			media[1] += imagem[((i*cols)+j)*3+1];
+			media[2] += imagem[((i*cols)+j)*3+2];
+			qtd++;
+		}
+	}
+
+	saida[indice*3] = (unsigned char)(media[0]/qtd);
+	saida[indice*3+1] = (unsigned char)(media[1]/qtd);
+	saida[indice*3+2] = (unsigned char)(media[2]/qtd);
+}
 
 void cudaColorido(char *nome_imagem, char *nome_saida)
 {
+	cv::Mat imagem = cv::imread(nome_imagem, CV_LOAD_IMAGE_COLOR); //abre a imagem de origem
+	cv::Mat saida(imagem.rows, imagem.cols, imagem.type()); //cria a imagem de destino
 	
+	struct timeval start,end;
+    double tempo=0.0;
+    gettimeofday(&start,NULL);
+	
+	unsigned char * imagem_entrada;
+	unsigned char * imagem_saida;
+	cudaMalloc((void **)&(imagem_entrada), sizeof(unsigned char) * imagem.rows*imagem.cols * 3);//aloca a imagem de origem na GPU
+	cudaMemcpy((void *)(imagem_entrada), (void *)(imagem.data), sizeof(unsigned char) * imagem.rows * imagem.cols * 3, cudaMemcpyKind::cudaMemcpyHostToDevice); //manda a imagem de origem pra GPU
+
+	cudaMalloc((void **)&(imagem_saida), sizeof(unsigned char) * imagem.rows*imagem.cols * 3);//aloca a imagem de destino na GPU
+	
+	/* Inicio do processo paralelo */
+	
+	cuInit(0); 
+	dim3 Bloco_dim(1024); //define bloco de 1 linha tamanho 1024
+
+
+
+	unsigned int num_Blocos = (unsigned int)ceil(((double)(imagem.rows*imagem.cols)) / 1024); //verifica quantos blocos serão necessário para a imagem
+
+	smoothGray << < (num_Blocos * 3), Bloco_dim >> > (imagem_entrada, imagem_saida, imagem.cols, imagem.rows); 
+	cudaDeviceSynchronize(); 
+
+	/* Fim do processo paralelo */
+	
+
+	cudaMemcpy((void *)(saida.data), (void *)imagem_saida, (size_t)(sizeof(unsigned char) * imagem.rows * imagem.cols * 3), cudaMemcpyKind::cudaMemcpyDeviceToHost); //copia a imagem resultante para o Host
+	
+	
+	gettimeofday(&end,NULL);
+    tempo =( ((double) ( ((end.tv_sec * 1000000 + end.tv_usec)
+                                - (start.tv_sec * 1000000 + start.tv_usec))))/1000000);
+
+    char nome[100];
+    sprintf(nome, "%s.out", imagem_saida);
+    FILE *fp = NULL;
+    if((fp = fopen(nome, "a")) == NULL)
+		fp = fopen(nome,"w");
+   
+    fprintf(fp, "%lf", tempo);
+    //apresentar os resultado
+    fclose(fp);
+	
+	
+	cv::imwrite(nome_saida, saida); //Escreve imagem no arquivo
+	
+	cudaFree(imagem_entrada);
+	cudaFree(imagem_saida);
 }
 
 int main()
@@ -107,10 +218,10 @@ int main()
 			int j = 0;
 			for(; j < i; ++j)
 			{
-				nome_saida[j] = nome_imagem[indice+j];
+				nome_saida[j] = nome_imagem[indice+j];//NICE
 			}
-			
-			sprintf (nome_saida, "saida/%s", nome_saida);
+
+			sprintf (nome_saida, "saida/%s", nome_imagem);
 			if(opcao_cor == 1){
 				cudaColorido(nome_imagem, nome_saida);
 			}else{
@@ -121,6 +232,7 @@ int main()
 		}
 		
 	}while(opcao_cor != 0);
+
 
     return 0;
 }
